fd3a9f69bf18ac98648786b264613e26071f3166
UppalaNagasreeRanjani <59391217+UppalaNagasreeRanjani@users.noreply.github.com>
2020-03-10 11:50:54 +0530

    Add files via upload

---
+++ TodayDate.java
@@ -0,0 +1,8 @@
+Import java.time.LocalDate;
+import java.time.LocalTime;
+public class TodayDate{
+LocalDate.date=LocalDate.now();
+System.our.println("today date is :"+date);
+LocalTime.time=LocalTime.now();
+System.our.println("today time is :"+time);
+}
