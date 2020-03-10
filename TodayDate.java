29d89ad76e2be177697a3925c7a7df3ae188feee
UppalaNagasreeRanjani <59391217+UppalaNagasreeRanjani@users.noreply.github.com>
2020-03-10 12:03:54 +0530

    Update TodayDate.java

--- TodayDate.java
+++ TodayDate.java
@@ -1,8 +1,8 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 public class TodayDate{
-LocalDate.date=LocalDate.now();
+LocalDate date=LocalDate.now();
 System.our.println("today date is :"+date);
-LocalTime.time=LocalTime.now();
+LocalTime time=LocalTime.now();
 System.our.println("today time is :"+time);
 }
